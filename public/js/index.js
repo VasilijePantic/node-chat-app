a26cd51c999ee5a7175b14e58034f194601fb32f
VasilijePantic <vasilijepantic@yahoo.com>
2018-04-03 18:59:17 +0200

    mustache.js for message templates added

--- public/js/index.js
+++ public/js/index.js
@@ -13,25 +13,31 @@ socket.on('disconnect', function () {
 // MESSAGES - custom newMessage event listener
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#message-template').html();
 
-    var li = $('<li></li>');// we are taking list item from index.html with jQuery
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);// adding -from and -message.text values to text propery in list
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    $('#messages').append(li);// appending values from list to OL
+    $('#messages').append(html);
+
 });
 
 
 //generateLocationMessage emmit
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#location-message-template').html();
 
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current Location</a>');
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    $('#messages').append(html);
 });
 
 
